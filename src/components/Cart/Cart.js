658aefcf06b3d22192ae28bbe5b87d624f4e2afa
shuvornoy <shuvornoy@gmaol.com>
2022-09-28 14:25:01 -0700

    cart style complrt

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Cart.css'
+import './Cart.css';
+import logo from '../../images/logo.png'
 
 const Cart = ({cart}) => {
 
@@ -10,8 +11,40 @@ const Cart = ({cart}) => {
     }
     return (
         <div className='cart'>
-            <h2>Shuvo Roy</h2>
-                <p>Exercise time: {total} seconds</p>
+            <div className='cart-logo'>
+            <img src={logo} alt="" />
+            <div className='cart-info'>
+                <h3>Shuvo Roy</h3>
+                <p>i am a developer</p>
+            </div>
+            </div>
+
+            <div className='cart-hight'>
+                <div><h3>53<small>kg</small></h3><p>Weight</p></div>
+                <div><h3>5.5</h3><p>Height</p></div>
+                <div><h3>75<small>yrs</small></h3><p>Age</p></div>
+            </div>
+
+            <div>
+                <h2>Add A Break</h2>
+                <div className='cart-break'>
+                    <button>10s</button>
+                    <button>10s</button>
+                    <button>10s</button>
+                    <button>10s</button>
+                </div>
+            </div>
+
+            <h3>Exercise Details</h3>
+            <div className='cart-time'>
+                <h4>Exercise time: {total} seconds</h4>
+            </div>
+            <div className='cart-time'>
+                <h4>Break time: seconds</h4>
+            </div>
+            <button className='cart-activity'>
+                <p>Activity Completed</p>
+            </button>
         </div>
     );
 };
